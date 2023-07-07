9a5f45d231151218119223a4c2261936bbad2a93
upsatwal <UPSAtwal@users.noreply.github.com>
2023-07-07 20:09:00 +0530

    change formatting of the js file

--- src/Pages/Gallery/Components/imageArea.js
+++ src/Pages/Gallery/Components/imageArea.js
@@ -1,5 +1,5 @@
 import classes from "./imageArea.module.css";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { useState } from "react";
 
 const ImageArea = (props) => {
@@ -14,20 +14,25 @@ const ImageArea = (props) => {
           fn={setIndexg}
           ig={indexg}
         />
+
         <RectSmall
           source={props.images[1]}
           index={2}
           fn={setIndexg}
           ig={indexg}
         />
+
         <RectBig
           source={props.images[2]}
           index={3}
           fn={setIndexg}
           ig={indexg}
         />
+
         <Sq source={props.images[3]} index={4} fn={setIndexg} ig={indexg} />
       </div>
+
+
       <div className={classes.c2}>
         <RectSmall
           source={props.images[4]}
@@ -35,17 +40,22 @@ const ImageArea = (props) => {
           fn={setIndexg}
           ig={indexg}
         />
+
         <Sq source={props.images[5]} index={6} fn={setIndexg} ig={indexg} />
+
         <RectBig
           source={props.images[6]}
           index={7}
           fn={setIndexg}
           ig={indexg}
         />
-        g
+
         <Sq source={props.images[7]} index={8} fn={setIndexg} ig={indexg} />
+
         <Sq source={props.images[8]} index={9} fn={setIndexg} ig={indexg} />
       </div>
+
+
       <div className={classes.c3}>
         <Sq source={props.images[9]} index={10} fn={setIndexg} ig={indexg} />
         <RectBig
@@ -54,12 +64,14 @@ const ImageArea = (props) => {
           fn={setIndexg}
           ig={indexg}
         />
+
         <RectSmall
           source={props.images[11]}
           index={12}
           fn={setIndexg}
           ig={indexg}
         />
+
         <RectBig
           source={props.images[12]}
           index={13}
@@ -74,40 +86,43 @@ const ImageArea = (props) => {
 export default ImageArea;
 
 const Sq = (props) => {
- 
+
   return (
     <img
       src={props.source}
-      style={props.ig === props.index?{}:{filter: "brightness(0.5) saturate(0%)"}}
+      style={props.ig === props.index ? {} : { filter: "brightness(0.5) saturate(0%)" }}
       className={classes.i3}
       onMouseEnter={() => props.fn(props.index)}
       onMouseOut={() => props.fn(-1)}
+      alt=""
     />
   );
 };
 
 const RectBig = (props) => {
-  
+
   return (
     <img
       src={props.source}
-      style={props.ig === props.index? {}:{filter: "brightness(0.5) saturate(0%)"}}
+      style={props.ig === props.index ? {} : { filter: "brightness(0.5) saturate(0%)" }}
       className={classes.i1}
       onMouseEnter={() => props.fn(props.index)}
       onMouseOut={() => props.fn(-1)}
+      alt=""
     />
   );
 };
 
 const RectSmall = (props) => {
-  
+
   return (
     <img
       src={props.source}
-      style={props.ig === props.index? {}:{filter: "brightness(0.5) saturate(0%)"}}
+      style={props.ig === props.index ? {} : { filter: "brightness(0.5) saturate(0%)" }}
       className={classes.i2}
       onMouseEnter={() => props.fn(props.index)}
       onMouseOut={() => props.fn(-1)}
+      alt=""
     />
   );
 };
