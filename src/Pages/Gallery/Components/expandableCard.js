23efc60b5cfa8e27dee84b5baa4f17b0c726e0c7
upsatwal <UPSAtwal@users.noreply.github.com>
2023-07-07 20:08:12 +0530

    comment out unnecessary variables

--- src/Pages/Gallery/Components/expandableCard.js
+++ src/Pages/Gallery/Components/expandableCard.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { motion } from "framer-motion";
 import { Fragment } from "react";
 import classes from "./expandableCard.module.css";
 import ImageArea from "./imageArea";
 
 const ExpandableCard = (props) => {
- 
+
   const wrpstl = {
     width: "80%",
     position: "relative",
@@ -24,16 +24,16 @@ const ExpandableCard = (props) => {
   return (
     <Fragment>
       <motion.div layout style={wrpstl} initial={wrpstl} animate={wrpstl}>
-      <motion.h2
-        className={classes.wrapper}
-        initial={classes.wrapper}
-        animate={classes.wrapper}
-        onClick={clickHandler}
-      >
-        {props.year}
-      </motion.h2>
-    
-        <motion.div>{props.index === props.actid && <ImageArea images={props.img}/>}</motion.div>
+        <motion.h2
+          className={classes.wrapper}
+          // initial={classes.wrapper}
+          // animate={classes.wrapper}
+          onClick={clickHandler}
+        >
+          {props.year}
+        </motion.h2>
+
+        <motion.div>{props.index === props.actid && <ImageArea images={props.img} />}</motion.div>
       </motion.div>
     </Fragment>
   );
